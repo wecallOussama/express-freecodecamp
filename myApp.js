460ad6fbc9a5836bd19642faeacd0393fdc6f6f7
wecallOussama <111273798+wecallOussama@users.noreply.github.com>
2022-08-15 11:56:23 +0000

    middleware chain

--- myApp.js
+++ myApp.js
@@ -1,24 +1,34 @@
 let express = require('express');
 let app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-let absolute_path = __dirname + '/views/index.html'
-let static_path = __dirname + '/public'
+let absolute_path = __dirname + '/views/index.html';
+let static_path = __dirname + '/public';
+let str = 'Hello json';
+
+app.use(function (req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+})
 
 app.use('/public', express.static(static_path))
 
 app.get('/', function (req, res) {
-  res.sendFile(absolute_path)
+  res.sendFile(absolute_path);
 })
 
 app.get('/json', function (req, res) {
-  let str = 'Hello json'
-  if(process.env.MESSAGE_STYLE == "uppercase") str = str.toUpperCase()
-  res.json({'message': str})
+  if(process.env.MESSAGE_STYLE == "uppercase") str = str.toUpperCase();
+  res.json({'message': str});
 })
 
-
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res){
+  res.json({'time': req.time});
+})
 
 
 
