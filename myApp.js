28f43f39d0a9a7ac6a6d717cf57f2466f289df1a
wecallOussama <111273798+wecallOussama@users.noreply.github.com>
2022-08-15 11:13:08 +0000

    json app get

--- myApp.js
+++ myApp.js
@@ -1,9 +1,18 @@
 let express = require('express');
 let app = express();
 
+let absolute_path = __dirname + '/views/index.html'
+let static_path = __dirname + '/public'
 
+app.use('/public', express.static(static_path))
 
+app.get('/', function (req, res) {
+  res.sendFile(absolute_path)
+})
 
+app.get('/json', function (req, res) {
+  res.json({'message': 'Hello json'})
+})
 
 
 
