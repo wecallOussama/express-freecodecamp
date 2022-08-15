9d0fcfb0953295e3ff86553eb36d53997f0c1a04
oussama <oussama@wecall.fr>
2022-08-15 12:18:35 +0100

    env var

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config()
+
 let absolute_path = __dirname + '/views/index.html'
 let static_path = __dirname + '/public'
 
@@ -11,7 +13,9 @@ app.get('/', function (req, res) {
 })
 
 app.get('/json', function (req, res) {
-  res.json({'message': 'Hello json'})
+  let str = 'Hello json'
+  if(process.env.MESSAGE_STYLE == "uppercase") str = str.toUpperCase()
+  res.json({'message': str})
 })
 
 
